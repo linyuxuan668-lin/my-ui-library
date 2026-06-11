3d097d8b5f6614fbb63d858b234b2554347a1fef
林昱瑄 <linyuxuan@MacBook-Air.local>
2026-06-11 10:50:21 +0800

    Updates

---
+++ assets/Button.stories-ChJXC8ou.js
@@ -0,0 +1,27 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,t as n}from"./Button-BmOePDUt.js";var r,i,a,o,s,c,l;e((()=>{t(),{fn:r}=__STORYBOOK_MODULE_TEST__,i={title:`Example/Button`,component:n,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{backgroundColor:{control:`color`}},args:{onClick:r()}},a={args:{primary:!0,label:`Button`},parameters:{design:{type:`figma`,url:`https://www.figma.com/design/za6zaaaFiWknqUR5xlhEDv/o-bank-App?node-id=14210-39587&t=nyM8QUXO0YT2WJOj-1`}}},o={args:{label:`Button`}},s={args:{size:`large`,label:`Button`}},c={args:{size:`small`,label:`Button`}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  },
+  // parameters
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/za6zaaaFiWknqUR5xlhEDv/o-bank-App?node-id=14210-39587&t=nyM8QUXO0YT2WJOj-1'
+    }
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Primary`,`Secondary`,`Large`,`Small`]}))();export{s as Large,a as Primary,o as Secondary,c as Small,l as __namedExportsOrder,i as default};
